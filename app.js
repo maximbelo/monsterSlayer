e99d29b0b333c3b7f775eae1aaa5ff2cafa0e9d3
maximbelo <mb@tineye.com>
2022-03-15 16:24:49 -0400

    Added logic to update health bars

--- app.js
+++ app.js
@@ -22,6 +22,15 @@ const app = Vue.createApp({
       this.playerHealth -= attackValue;
     },
   },
+  computed: {
+    monsterBarStyles() {
+      return { width: this.monsterHealth + "%" };
+    },
+
+    playerBarStyles() {
+      return { width: this.playerHealth + "%" };
+    },
+  },
 });
 
 app.mount("#game");
