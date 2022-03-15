146b298c653eef3bbfc512db072a3387a5e85df7
maximbelo <mb@tineye.com>
2022-03-15 16:42:56 -0400

    Added special attack logic

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
     };
   },
 
   methods: {
     attackMonster() {
+      this.currentRound++;
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
@@ -21,6 +23,13 @@ const app = Vue.createApp({
       const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
     },
+
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
   },
   computed: {
     monsterBarStyles() {
@@ -29,6 +38,10 @@ const app = Vue.createApp({
 
     playerBarStyles() {
       return { width: this.playerHealth + "%" };
+    },
+
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
     },
   },
 });
