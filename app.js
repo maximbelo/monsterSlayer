defe749e012fe252963961a431aad37bb07005d9
maximbelo <mb@tineye.com>
2022-03-16 12:49:13 -0400

    Finalized core game func

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ const app = Vue.createApp({
   },
 
   methods: {
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.currentRound = 0;
+      this.winner = null;
+    },
+
     attackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(5, 12);
@@ -42,13 +49,25 @@ const app = Vue.createApp({
       }
       this.attackPlayer();
     },
+
+    surrender() {
+      this.winner = "monster";
+    },
   },
+
   computed: {
     monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: "0%" };
+      }
       return { width: this.monsterHealth + "%" };
     },
 
     playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: "0%" };
+      }
+
       return { width: this.playerHealth + "%" };
     },
 
